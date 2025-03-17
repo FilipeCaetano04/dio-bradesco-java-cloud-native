fbcc69993980256295f079c5c406feffac309ef6
Filipe Caetano <filipekaetano04@gmail.com>
2025-03-17 19:45:11 -0300

    Criação da lógica para entrada de dados  pelo terminal esaída da mensagem

--- desafios/sintaxe/ContaBanco/src/ContaTerminal.java
+++ desafios/sintaxe/ContaBanco/src/ContaTerminal.java
@@ -1,2 +1,35 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(Locale.US); // Define o local de scanner para aceitar '.' como representação decimal
+
+        int agencia;
+        String numeroConta;
+        String nomeCliente;
+        double saldo;
+
+        System.out.println("Por favor, informe as informações necessárias!");
+
+        System.out.println("Digite o número da Agência!");
+        agencia = scanner.nextInt();
+        scanner.nextLine(); // Limpar o buffer
+
+        System.out.println("Digite o número da Conta!");
+        numeroConta = scanner.nextLine();
+
+        System.out.println("Digite o nome do Cliente!");
+        nomeCliente = scanner.nextLine();
+
+        System.out.println("Digite o Saldo da conta!");
+        saldo = scanner.nextDouble();
+        scanner.nextLine(); // Limpar o buffer
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, " +
+                           "sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo "  +
+                            saldo + " já está disponível para saque.");
+
+    }
 }
